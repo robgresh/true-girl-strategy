a7217b6a0105539eba2531d88fa5bfcb8d2f2a37
Robby Gresh <robby@MacBookPro.localdomain>
2026-02-28 01:09:25 -0500

    feat: Add scroll morph hero interactive component

---
+++ src/components/demo.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import IntroAnimation from "./ui/scroll-morph-hero";
+
+export default function Demo() {
+    return (
+        <div className="w-full h-[800px] border shadow-2xl rounded-2xl overflow-hidden relative mt-10">
+            <IntroAnimation />
+        </div>
+    );
+}
